f890aef1e451ff30519786e9d25c5559198633e6
Khomendra <khomendrakumar.ak.94@gmail.com>
2024-02-11 17:11:17 +0530

    Pagination added

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -4,6 +4,7 @@ import com.springboot.blog.entity.Post;
 import com.springboot.blog.payload.PostDTO;
 import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
+import com.springboot.blog.utils.AppConstants;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -32,10 +33,12 @@ public class PostController {
     //Get all posts
     @GetMapping
     public PostResponse getAllPosts(
-                @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-                @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize
+                @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+                @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+                @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+                @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
                 ){
-        return postService.getAllPosts(pageNo, pageSize);
+        return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
     @GetMapping("/{id}")
